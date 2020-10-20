b00d4b21e07039a1b2da53b1930484e9a8cfec20
Riccardo Graziosi <riccardo.graziosi97@gmail.com>
2020-10-20 11:58:52 +0200

    Add reproducible_results script

---
+++ reproducible_results.py
@@ -0,0 +1,21 @@
+# Adapted from: https://stackoverflow.com/a/52897289
+
+# Seed value
+# Apparently you may use different seed values at each stage
+seed_value= 1
+
+# 1. Set `PYTHONHASHSEED` environment variable at a fixed value
+import os
+os.environ['PYTHONHASHSEED']=str(seed_value)
+
+# 2. Set `python` built-in pseudo-random generator at a fixed value
+import random
+random.seed(seed_value)
+
+# 3. Set `numpy` pseudo-random generator at a fixed value
+import numpy as np
+np.random.seed(seed_value)
+
+# 4. Set the `tensorflow` pseudo-random generator at a fixed value
+import tensorflow as tf
+tf.random.set_seed(seed_value)
