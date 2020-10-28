9a11d511ec3a10c9dd9448366000b807b34227b1
Riccardo Graziosi <riccardo.graziosi97@gmail.com>
2020-10-28 15:17:19 +0100

    Add validation split experiment script

--- experiments_validation_split.py
+++ experiments_validation_split.py
@@ -1,36 +1,30 @@
-import reproducible_results
 from config import config, hyperparams, network
 from nn import train, evaluate
-
-network['model_fit_callbacks'] = []
-hyperparams['epochs'] = 10
+import matplotlib.pyplot as plt
 
 histories = []
-losses = []
 
 splits = [0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5]
 
 for validation_split in splits:
-  print("Validation Split ", validation_split)
+  print("----- Validation Split ", validation_split, "-----")
 
   hyperparams['validation_split'] = validation_split
 
   model, history = train(config, hyperparams, network)
-  loss = evaluate(model, config)
 
-  histories.append(history)
-  losses.append(loss)
+  histories.append(history.history['val_loss'])
 
-
-print("Losses: ", losses)
-
-import matplotlib.pyplot as plt
+with open('experiments_validation_split_output.txt', 'w') as f:
+  print("validation splits:", splits, file=f)
+  print("val_loss histories:", histories, file=f)
 
 for i in range(len(splits)):
-  plt.plot(histories[i].history['val_accuracy'])
+  plt.plot(histories[i])
 
-plt.title('Validation accuracy as validation split changes')
-plt.ylabel('accuracy')
+plt.title('Validation loss as validation split changes')
+plt.ylabel('loss')
 plt.xlabel('epoch')
-plt.legend(["{}".format(split) for split in splits], loc='upper left')
+plt.legend(["{}".format(split) for split in splits], loc='upper right')
+plt.savefig('experiments_validation_split_plot.png')
 plt.show()
