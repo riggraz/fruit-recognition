2bb43b3811994db95fe12e1e6d919de815792866
Riccardo Graziosi <riccardo.graziosi97@gmail.com>
2020-10-28 15:17:27 +0100

    Add optimizer experiment script

--- experiments_optimizer.py
+++ experiments_optimizer.py
@@ -1,18 +1,8 @@
-# import reproducible_results
 from config import config, hyperparams, network
 from nn import train, evaluate
-
-from tensorflow.keras import optimizers
-
 import matplotlib.pyplot as plt
-import numpy
-
-hyperparams['batch_size'] = 64
 
 histories = []
-avg_losses = []
-all_losses = []
-variances = []
 
 optimizers = [
   'adam',
@@ -26,65 +16,27 @@ learning_rates = [
   0.01,
 ]
 
-def calc_test_error(repetitions=10):
-  for i in range(len(optimizers)):
-    hyperparams['optimizer'] = optimizers[i]
-    hyperparams['learning_rate'] = learning_rates[i]
+for i in range(len(optimizers)):
+  print("---------", "optimizer", optimizers[i], "---------")
 
-    current_losses = []
+  hyperparams['optimizer'] = optimizers[i]
+  hyperparams['learning_rate'] = learning_rates[i]
 
-    for j in range(repetitions):
-      print("---------", "optimizer", optimizers[i], ", repetition", j, "---------")
+  model, history = train(config, hyperparams, network)
 
-      model, history = train(config, hyperparams, network)
-      loss = evaluate(model, config)
+  histories.append(history.history['val_loss'])
 
-      current_losses.append(loss)
+with open('experiments_optimizers_output.txt', 'w') as f:
+  print("optimizers:", optimizers, file=f)
+  print("learning rates:", learning_rates, file=f)
+  print("val_loss histories:", histories, file=f)
 
-    all_losses.append(current_losses)
-    avg_losses.append(numpy.average(current_losses))
-    variances.append(numpy.var(current_losses))
+for i in range(len(optimizers)):
+  plt.plot(histories[i])
 
-  print("optimizers:", optimizers)
-  print("learning rates:", learning_rates)
-  print("avg losses:", avg_losses)
-  print("variances:", variances)
-  print("all losses:", all_losses)
-
-  with open('experiments_optimizer_output.txt', 'w') as f:
-    print("optimizers:", optimizers, file=f)
-    print("learning rates:", learning_rates, file=f)
-    print("avg losses:", avg_losses, file=f)
-    print("variances:", variances, file=f)
-    print("all losses:", all_losses, file=f)
-
-def plot_val_acc(epochs=100):
-  tmp = (network['model_fit_callbacks'], hyperparams['epochs'])
-  network['model_fit_callbacks'] = []
-  hyperparams['epochs'] = epochs
-
-  for i in range(len(optimizers)):
-    print("---------", "optimizer", optimizers[i], "---------")
-
-    hyperparams['optimizer'] = optimizers[i]
-    hyperparams['learning_rate'] = learning_rates[i]
-
-    model, history = train(config, hyperparams, network)
-
-    histories.append(history)
-
-  for i in range(len(optimizers)):
-    plt.plot(histories[i].history['val_accuracy'])
-
-  plt.title('Validation accuracy as optimizer changes')
-  plt.ylabel('accuracy')
-  plt.xlabel('epoch')
-  plt.legend(["{}".format(optimizer) for optimizer in optimizers], loc='upper left')
-  plt.savefig('experiments_optimizer_plot.png')
-  plt.show()
-
-  # restore previous values
-  network['model_fit_callbacks'], hyperparams['epochs'] = tmp
-
-calc_test_error(repetitions=10)
-plot_val_acc(epochs=5)
+plt.title('Validation loss as optimizer changes')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(["{}".format(optimizer) for optimizer in optimizers], loc='upper right')
+plt.savefig('experiments_optimizer_plot.png')
+plt.show()
